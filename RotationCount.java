97f9799893c0aa7cc7a7da29aa865009e7e0e00c
akshu88 <akshitaag904@gmail.com>
2025-06-30 13:12:27 +0530

    Refactor RBS class to replace findPivotwithuplicates with findPivot method; add RotationCount and SplitArray classes with rotation counting and array splitting logic

--- RotationCount.java
+++ RotationCount.java
@@ -1,47 +1,15 @@
-public class RBS {
+public class RotationCount {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 6, 7, 0, 1, 2, 2, 4};
-        // System.out.println(findPivotwithuplicates(arr));
-        System.out.println(findPivot(arr));
+        // int[] arr = {1,2,3,4,5,6};
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(countRotation(arr));
     }
-
-    static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-
-        // if you did not find a pivot, it means the array is not rotated
-        if(pivot == -1){
-            // just do normal binary search
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        // if pivot is found, you have found 2 asc sorted arrays
-        if(nums[pivot] == target){
-            return pivot;
-        }
-        if(target >= nums[0]){
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    private static int countRotation(int[] arr) {
+        int pivot = findPivot(arr);
+        return pivot + 1;
     }
-
-    static int binarySearch(int[] arr, int target, int start, int end){
-        while (start <= end) {
-            // find the middle element
-            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2; 
-            
-            if(target < arr[mid]){
-                end = mid -1;
-            } else if(target > arr[mid]){
-                start = mid + 1; 
-            } else{
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-
+    
+    // use this for non-duplicates
     static int findPivot(int[] arr){
         int start = 0;
         int end = arr.length - 1;
@@ -53,7 +21,7 @@ public class RBS {
                 return mid;
             }
             if(mid > start && arr[mid] < arr[mid - 1]){
-                return mid - 1;
+                return mid;
             }
             if(arr[mid] <= arr[start]){
                 end = mid - 1;
@@ -64,7 +32,7 @@ public class RBS {
         return -1;
     }
 
-    /* 
+    // use this when array contains duplicates
     static int findPivotwithuplicates(int[] arr){
         int start = 0;
         int end = arr.length - 1;
@@ -76,8 +44,7 @@ public class RBS {
             }
             if(mid > start && arr[mid] < arr[mid - 1]){
                 return mid;
-            }
-            
+            }       
             // if elements at middle, start, end are equal then just skip the duplicates
             if(arr[mid] == arr[start] && arr[mid] == arr[end]){
                 // skip the duplicates
@@ -87,7 +54,6 @@ public class RBS {
                     return start;
                 }
                 start++;
-
                 // check whether end is pivot
                 if(arr[end] < arr[end - 1]){
                     return end - 1;
@@ -103,5 +69,4 @@ public class RBS {
         }
         return -1;
     }
-    */
 }
