796940860671346cbe6d8b5041a8e2c214c1bf23
akshu88 <akshitaag904@gmail.com>
2025-07-06 23:40:05 +0530

    Fix typo in InsertionSort method name and implement DisappearNumber, FindAllDuplicates, FindDuplicate, FirstMissingPositive, and SetMismatch classes with respective logic

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,13 +1,12 @@
 import java.util.Arrays;
-
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {5,3,4,1,2};
-        inseration(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void inseration(int[] arr){
+    static void insertion(int[] arr){
         for(int i  = 0; i < arr.length -1; i++){
             for(int j = i + 1; j > 0; j--){
                 if(arr[j] < arr[j - 1]){
