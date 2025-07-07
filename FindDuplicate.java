1705a2928cefd14b69463bb852dcde68d18a6b97
akshu88 <akshitaag904@gmail.com>
2025-07-08 00:35:11 +0530

    Remove unnecessary blank line in findDuplicate method

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -6,7 +6,6 @@ public class FindDuplicate {
     public static int findDuplicate(int[] arr) {
         int i = 0;
         while(i < arr.length){
-
             if (arr[i] != i + 1) {
                 int correct = arr[i] - 1; 
                 if(arr[i] != arr[correct]){
